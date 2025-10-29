1c4ff6570c0bf3e8d2a4b3267c080bd8f6a826fc
QDong26 <s3938006@rmit.edu.vn>
2025-10-29 13:54:52 +0700

    add ignored file into git ignore

--- BubbleSort/src/main/java/redo/tut1/BubbleSort.java
+++ BubbleSort/src/main/java/redo/tut1/BubbleSort.java
@@ -1,5 +1,17 @@
 package redo.tut1;
 
 public class BubbleSort {
-
+    public static Integer[] sort(Integer[] array) {
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        return array;
+    }
 }
